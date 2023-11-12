b5c6edca89ea12bbf18bf98d4db9b3ad6b5ab408
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-11-12 21:54:22 +0530

    added user reg view

--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from .forms import RegistrationForm
 
 # Create your views here.
+
+
+class RegistrationView(CreateView):
+    # Create an HTML template for registration
+    template_name = 'registration/register.html'
+    form_class = RegistrationForm
+
+    def form_valid(self, form):
+        # Log in the user after successful registration
+        login(self.request, form.instance)
+        return super().form_valid(form)
