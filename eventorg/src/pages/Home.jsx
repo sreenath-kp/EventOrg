13ba3b0eb23c85d1d4163a196dd08aca03948843
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2023-11-13 01:07:51 +0530

    homescreen

--- eventorg/src/pages/Home.jsx
+++ eventorg/src/pages/Home.jsx
@@ -7,13 +7,6 @@ export const Home = () => {
   return (
     <div>
       <Hero />
-      <EventTile
-        image="https://atneventstaffing.com/wp-content/uploads/2018/08/2018_Lipton_Cherry-Fest_Travese-City-MI_Brand-Ambassadors-.jpg.webp"
-        title="Event 1"
-        date="12/12/12"
-        time="12:12"
-        location="Location 1"
-      />
       <h3>Upcoming Events</h3>
       <EventScroll />
       <h3>Past Events</h3>
